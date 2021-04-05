1cc997bece9b55064fd239b0e249d0a499c23969
VaganovAnil <70805522+VaganovAnil@users.noreply.github.com>
2021-04-05 14:52:40 +0500

    init: start training-task

---
+++ Training/script.js
@@ -0,0 +1,16 @@
+module.exports = function toReadable(number) {
+    var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'
+    ];
+    var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+        'ninety'
+    ];
+
+    var numberString = number.toString();
+
+    if (number < 0) throw new Error('Negative numbers are not supported.');
+
+    if (number === 0) return 'zero';
+
+};
